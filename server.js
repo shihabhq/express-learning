39b884635bf921ae22bb693832bd9330aee70323
shihab <adnansaim15@yahoo.com>
2024-07-29 16:40:31 +0600

    post method on the server created

--- server.js
+++ server.js
@@ -15,16 +15,31 @@ app.get("/api/posts", (req, res) => {
 
 app.param("id", (req, res, next, id) => {
   const foundPost = posts.find((post) => post.id === parseInt(id));
-    req.currentObject = foundPost;
+  req.currentObject = foundPost;
   next();
 });
 
 app.get("/api/posts/:id", (req, res) => {
-    console.log(req.params)
+  console.log(req.params);
   if (req.currentObject) {
     res.json(req.currentObject);
   } else {
     res.status(400).send("requested user not found");
+  }
+});
+
+app.use(express.text());
+
+app.post("/api/posts", (req, res) => {
+  if (req.body && req.body.length) {
+    let newPost = {
+      id: posts.length + 1,
+      post: req.body,
+    };
+    posts.push(newPost);
+    res.send("posted successfully");
+  } else {
+    res.status(400).send("no post given");
   }
 });
 
